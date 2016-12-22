f7b831543124d93ed4abbcdafa3ce93f5deec1cf
Sam S <lytefast@gmail.com>
2016-12-21 19:16:01 -0800

    Initial commit

---
+++ app/src/main/java/com/lytefast/fancyinput/widget/FancyInput.java
@@ -0,0 +1,132 @@
+package com.lytefast.fancyinput.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatImageButton;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.lytefast.fancyinput.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class FancyInput extends RelativeLayout {
+    private String mExampleString; // TODO: use a default from R.string...
+    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
+    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
+    private Drawable mExampleDrawable;
+
+    @BindView(R.id.emoji_container) View emojiContainer;
+    @BindView(R.id.text_input) AppCompatEditText textEt;
+
+    public FancyInput(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public FancyInput(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public FancyInput(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        initAttributes(attrs, defStyle);
+        inflate(getContext(), R.layout.fancy_input_wrapper, this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        ButterKnife.bind(this);
+
+        if (!TextUtils.isEmpty(mExampleString)) {
+            textEt.setText(mExampleString);
+        }
+    }
+
+    private void initAttributes(final AttributeSet attrs, final int defStyle) {
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.FancyInput, defStyle, 0);
+
+        mExampleString = a.getString(
+                R.styleable.FancyInput_exampleString);
+        mExampleColor = a.getColor(
+                R.styleable.FancyInput_exampleColor,
+                mExampleColor);
+        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
+        // values that should fall on pixel boundaries.
+        mExampleDimension = a.getDimension(
+                R.styleable.FancyInput_exampleDimension,
+                mExampleDimension);
+
+        if (a.hasValue(R.styleable.FancyInput_exampleDrawable)) {
+            mExampleDrawable = a.getDrawable(
+                    R.styleable.FancyInput_exampleDrawable);
+            mExampleDrawable.setCallback(this);
+        }
+
+        a.recycle();
+    }
+
+    /**
+     * Gets the example string attribute value.
+     *
+     * @return The example string attribute value.
+     */
+    public String getExampleString() {
+        return textEt.getText().toString();
+    }
+
+    /**
+     * Sets the view's example string attribute value. In the example view, this string
+     * is the text to draw.
+     *
+     * @param exampleString The example string attribute value to use.
+     */
+    public void setExampleString(String exampleString) {
+        textEt.setText(exampleString);
+    }
+
+    @OnClick(R.id.send_btn)
+    void onSend() {
+        // TODO: figure out a way to publish events
+        Toast.makeText(getContext(), "Text Sent: " + textEt.getText().toString(), Toast.LENGTH_SHORT).show();
+        textEt.setText("");
+    }
+
+    @OnClick(R.id.emoji_btn)
+    void onEmojiToggle(AppCompatImageButton btn) {
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (emojiContainer.getVisibility() == VISIBLE) {
+            emojiContainer.setVisibility(GONE);
+            imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT);
+            btn.setImageResource(R.drawable.ic_insert_emoticon_24dp);
+        } else {
+            emojiContainer.setVisibility(VISIBLE);
+            imm.hideSoftInputFromWindow(this.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            btn.setImageResource(R.drawable.ic_keyboard_24dp);
+        }
+    }
+}
