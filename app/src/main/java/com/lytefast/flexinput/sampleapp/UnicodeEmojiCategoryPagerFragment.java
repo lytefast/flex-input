1c3abfefad24234587922d9607dacb67b52d0c3c
Sam S <lytefast@gmail.com>
2017-01-11 16:40:56 -0800

    add category icons

--- app/src/main/java/com/lytefast/flexinput/sampleapp/UnicodeEmojiCategoryPagerFragment.java
+++ app/src/main/java/com/lytefast/flexinput/sampleapp/UnicodeEmojiCategoryPagerFragment.java
@@ -1,5 +1,6 @@
 package com.lytefast.flexinput.sampleapp;
 
+import android.support.annotation.DrawableRes;
 import android.util.JsonReader;
 import android.util.Log;
 
@@ -67,7 +68,7 @@ public class UnicodeEmojiCategoryPagerFragment extends EmojiCategoryPagerFragmen
     emojis.trimToSize();
 
     jsonReader.endArray();
-    return new Emoji.EmojiCategory(name, R.drawable.ic_audiotrack_light, emojis);
+    return new Emoji.EmojiCategory(name, getCategoryIcon(name), emojis);
   }
 
   private Emoji readEmoji(final JsonReader jsonReader) throws IOException {
@@ -96,5 +97,29 @@ public class UnicodeEmojiCategoryPagerFragment extends EmojiCategoryPagerFragmen
 
     jsonReader.endObject();
     return new Emoji(strValue, aliases.toArray(new String[aliases.size()]));
+  }
+
+  @DrawableRes
+  private static int getCategoryIcon(String categoryName) {
+    switch (categoryName) {
+      case "people":
+        return R.drawable.ic_mood_black_24dp;
+      case "nature":
+        return R.drawable.ic_local_florist_black_24dp;
+      case "food":
+        return R.drawable.ic_local_pizza_black_24dp;
+      case "activity":
+        return R.drawable.ic_beach_access_black_24px;
+      case "travel":
+        return R.drawable.ic_directions_car_black_24dp;
+      case "objects":
+        return R.drawable.ic_stars_black_24dp;
+      case "symbols":
+        return R.drawable.ic_create_black_24dp;
+      case "flags":
+        return R.drawable.ic_assistant_photo_black_24dp;
+      default:
+        return R.drawable.ic_audiotrack_light;
+    }
   }
 }
