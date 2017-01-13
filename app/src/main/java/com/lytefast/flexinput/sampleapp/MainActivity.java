2778ff635ffcc30b88858e005250c24e12f40f03
Sam S <lytefast@gmail.com>
2017-01-12 17:07:58 -0800

    add day night theme to show off some of the custom stylings

--- app/src/main/java/com/lytefast/flexinput/sampleapp/MainActivity.java
+++ app/src/main/java/com/lytefast/flexinput/sampleapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.lytefast.flexinput.sampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -16,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     // This should be in the Application class so it only gets invoked once
     Fresco.initialize(this);
+
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
   }
 }
