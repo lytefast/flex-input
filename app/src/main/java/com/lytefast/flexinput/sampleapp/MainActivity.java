8c1ba5f35a4fc1a267b0927aca0112b62e627457
Sam S <lytefast@gmail.com>
2017-02-07 13:03:09 -0800

    update styles to be more flexible

--- app/src/main/java/com/lytefast/flexinput/sampleapp/MainActivity.java
+++ app/src/main/java/com/lytefast/flexinput/sampleapp/MainActivity.java
@@ -4,6 +4,8 @@ package com.lytefast.flexinput.sampleapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.SwitchCompat;
+import android.widget.CompoundButton;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -21,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
     // Play around with the below to see how it looks like in each theme
 //     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+    SwitchCompat nightModeToggle = new SwitchCompat(this, null);
+    nightModeToggle.setChecked(true);
+    nightModeToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
+        AppCompatDelegate.setDefaultNightMode(
+            isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
+      }
+    });
+    getSupportActionBar().setCustomView(nightModeToggle);
   }
 }
