ff4c9e4dd8a04ea93c3973bbfa9bce9f37434ad8
Sam S <lytefast@gmail.com>
2017-09-22 01:12:07 -0700

    Kotlinfy Adapters

---
+++ flexinput/src/main/java/com/lytefast/flexinput/adapters/PhotoCursorAdapter.kt
@@ -0,0 +1,159 @@
+package com.lytefast.flexinput.adapters
+
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.content.AsyncQueryHandler
+import android.content.ContentResolver
+import android.database.Cursor
+import android.net.Uri
+import android.provider.MediaStore
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.facebook.drawee.view.SimpleDraweeView
+import com.lytefast.flexinput.R
+import com.lytefast.flexinput.model.Photo
+import com.lytefast.flexinput.utils.SelectionCoordinator
+
+
+/**
+ * [android.support.v7.widget.RecyclerView.Adapter] that knows how to load photos from the media store.
+ *
+ * @author Sam Shih
+ */
+class PhotoCursorAdapter(private val contentResolver: ContentResolver,
+                         selectionCoordinator: SelectionCoordinator<in Photo>)
+  : RecyclerView.Adapter<PhotoCursorAdapter.ViewHolder>() {
+  private val selectionCoordinator: SelectionCoordinator<in Photo> = selectionCoordinator.bind(this)
+  private var cursor: Cursor? = null
+
+  private var colId: Int = 0
+  private var colData: Int = 0
+  private var colName: Int = 0
+
+
+  init {
+    setHasStableIds(true)
+  }
+
+  override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
+    super.onAttachedToRecyclerView(recyclerView)
+    loadPhotos()
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoCursorAdapter.ViewHolder {
+    val view = LayoutInflater.from(parent.context)
+        .inflate(R.layout.view_grid_image, parent, false)
+    return ViewHolder(view)
+  }
+
+  override fun onBindViewHolder(holder: PhotoCursorAdapter.ViewHolder, position: Int) {
+    val photo = this[position]
+    holder.bind(photo)
+  }
+
+  override fun getItemCount(): Int {
+    return cursor?.count ?: 0
+  }
+
+  override fun getItemId(position: Int): Long {
+    return this[position].id
+  }
+
+  override fun onDetachedFromRecyclerView(recyclerView: RecyclerView?) {
+    cursor?.close()
+    super.onDetachedFromRecyclerView(recyclerView)
+  }
+
+  fun loadPhotos() {
+    val asyncQueryHandler = object : AsyncQueryHandler(contentResolver) {
+      override fun onQueryComplete(token: Int, cookie: Any, cursor: Cursor?) {
+        if (cursor == null) {
+          return
+        }
+        this@PhotoCursorAdapter?.apply {
+          this.colId = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+          this.colData = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+          this.colName = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+          this.cursor = cursor
+        }
+        notifyDataSetChanged()
+      }
+    }
+    asyncQueryHandler.startQuery(1, this,
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        arrayOf(MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DATA,
+            MediaStore.Images.Media.DISPLAY_NAME),
+        null, null, "${MediaStore.Images.Media.DATE_ADDED} DESC")
+  }
+
+  private operator fun get(position: Int): Photo {
+    cursor!!.moveToPosition(position)
+    val photoId = Integer.toString(cursor!!.getInt(colId))
+    val fileUri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, photoId)
+    return Photo(
+        cursor!!.getLong(colId), fileUri, cursor!!.getString(colName), cursor!!.getString(colData))
+  }
+
+  inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    private val shrinkAnim: AnimatorSet
+    private val growAnim: AnimatorSet
+
+    private val imageView: SimpleDraweeView = itemView.findViewById(R.id.content_iv)
+    private val checkIndicator: SimpleDraweeView = itemView.findViewById(R.id.item_check_indicator)
+
+    private var photo: Photo? = null
+
+
+    init {
+      this.itemView.setOnClickListener(this)
+      //region Perf: Load animations once
+      this.shrinkAnim = AnimatorInflater.loadAnimator(
+          itemView.context, R.animator.selection_shrink) as AnimatorSet
+      this.shrinkAnim.setTarget(imageView)
+
+      this.growAnim = AnimatorInflater.loadAnimator(
+          itemView.context, R.animator.selection_grow) as AnimatorSet
+      this.growAnim.setTarget(imageView)
+      //endregion
+    }
+
+    fun bind(photo: Photo) {
+      this.photo = photo
+      setSelected(selectionCoordinator.isSelected(photo, adapterPosition), false)
+
+      val thumbnailUri = photo.getThumbnailUri(contentResolver)
+      if (thumbnailUri == null) {
+        imageView.setImageURI(null as String?)
+      } else {
+        imageView.setImageURI(thumbnailUri)
+      }
+    }
+
+    internal fun setSelected(isSelected: Boolean, isAnimationRequested: Boolean) {
+      itemView.isSelected = isSelected
+
+      fun scaleImage(animation: AnimatorSet) {
+        animation.start()
+        if (!isAnimationRequested) {
+          animation.end()
+        }
+      }
+
+      if (isSelected) {
+        checkIndicator.visibility = View.VISIBLE
+        if (imageView.scaleX == 1.0f) scaleImage(shrinkAnim)
+      } else {
+        checkIndicator.visibility = View.GONE
+        if (imageView.scaleX != 1.0f) scaleImage(growAnim)
+      }
+    }
+
+    override fun onClick(v: View) {
+      setSelected(selectionCoordinator.toggleItem(photo, adapterPosition), true)
+    }
+  }
+}
