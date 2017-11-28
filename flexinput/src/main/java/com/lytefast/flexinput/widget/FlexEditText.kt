9b904c25a7ab3e1a1f717081706e7a9eb0ea94fb
Sam S <lytefast@gmail.com>
2017-11-28 12:57:52 -0800

    Fix: make sure that IC is nonnull before trying to wrap it

--- flexinput/src/main/java/com/lytefast/flexinput/widget/FlexEditText.kt
+++ flexinput/src/main/java/com/lytefast/flexinput/widget/FlexEditText.kt
@@ -28,24 +28,24 @@ open class FlexEditText @JvmOverloads constructor(
    */
   var inputContentHandler: ((InputContentInfoCompat) -> Unit)? = null
 
-  override fun onCreateInputConnection(editorInfo: EditorInfo): InputConnection {
-    val ic = super.onCreateInputConnection(editorInfo)
-    EditorInfoCompat.setContentMimeTypes(editorInfo, arrayOf("image/*"))
+  override fun onCreateInputConnection(editorInfo: EditorInfo): InputConnection? =
+    super.onCreateInputConnection(editorInfo)?.let {
+      EditorInfoCompat.setContentMimeTypes(editorInfo, arrayOf("image/*"))
 
-    val callback = InputConnectionCompat.OnCommitContentListener { inputContentInfo, flags, _ ->
-      // read and display inputContentInfo asynchronously
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
-          && (flags and InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION ) != 0) {
-        try {
-          inputContentInfo.requestPermission()
-        } catch (e: Exception) {
-          return@OnCommitContentListener false // return false if failed
+      val callback = InputConnectionCompat.OnCommitContentListener { inputContentInfo, flags, _ ->
+        // read and display inputContentInfo asynchronously
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+            && (flags and InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+          try {
+            inputContentInfo.requestPermission()
+          } catch (e: Exception) {
+            return@OnCommitContentListener false // return false if failed
+          }
         }
-      }
 
-      inputContentHandler?.invoke(inputContentInfo)
-      true  // return true if succeeded
+        inputContentHandler?.invoke(inputContentInfo)
+        true  // return true if succeeded
+      }
+      InputConnectionCompat.createWrapper(it, editorInfo, callback)
     }
-    return InputConnectionCompat.createWrapper(ic, editorInfo, callback)
-  }
 }
